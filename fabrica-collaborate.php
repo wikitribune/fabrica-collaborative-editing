3c594c4804843760c9c5cae4f569b7fcc7ec4271
Andrew Staffell <andrew@yeswework.com>
2017-08-29 22:13:59 +0100

    use post meta to store editor of latest post; other changes

--- fabrica-collaborate.php
+++ fabrica-collaborate.php
@@ -6,7 +6,7 @@ Description:
 Version: 0.0.1
 Author: Fabrica
 Author URI: https://fabri.ca/
-Text Domain: fabrica-dashboard
+Text Domain: fabrica-collaborate
 License: GPL-2.0+
 License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 */
@@ -51,35 +51,44 @@ class Plugin {
 
 	public function resolveEditConflicts($data, $postarr) {
 
-		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) { return $data; }
+		// if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) { return $data; }
+		if ($postarr['ID'] == 0) { return $data; }
 
 		// Retrieve the saved version of the post being edited
 		$savedPost = get_post($postarr['ID'], ARRAY_A);
 
 		// Get the user ID of the last published version
-		$latestEditor = $savedPost['post_author'];
+		$latestEditor = get_post_meta($postarr['ID'], '_latest_editor', true);
 
-		// Transient name
+		// If no latest editor, no one has made an edit yet, so save one now and leave
+		if (!$latestEditor) {
+			update_post_meta($postarr['ID'], '_latest_editor', get_current_user_id());
+			return $data;
+		}
+
+		// If published editor is current user, they're just editing their own content, so leave
+		if ($latestEditor == get_current_user_id()) {
+			return $data;
+		}
+
+		// Define transient where we store the edit in case of a clash
 		$transient = 'conflict_' . $postarr['ID'] . '_' . get_current_user_id();
 
-		// Set the post_author to the current user as a way of keeping track of which author did the last accepted edit
-		// the _edit_last post_meta is no good because by this point it's already been changed
-		// TODO: probably use our own post_meta instead, to avoid interfering with WP core stuff
-		$data['post_author'] = get_current_user_id();
-
 		// Check if there's a merge conflict between the saved version and the current version (and the authors are different)
-		if ($latestEditor != get_current_user_id()) {
-			if (get_transient($transient)) { // This means the revision has already been saved once, so let it through the second time
-				delete_transient($transient);
-			} else if (wp_text_diff($savedPost['post_content'], $data['post_content'])) {
+		if (get_transient($transient)) {
 
-				// Save the conflicted data in a transient based on the current post ID and current author ID
-				set_transient($transient, stripslashes($data['post_content']), WEEK_IN_SECONDS);
+			// This means the revision has already been saved once, so let it through the second time
+			// TODO: add more sanitization and checks here, as well as more sophisticated controls for merge resolution
+			update_post_meta($postarr['ID'], '_latest_editor', get_current_user_id());
+			delete_transient($transient);
 
-				// Revert to saved version
-				$data['post_author'] = $savedPost['post_author'];
-				$data['post_content'] = $savedPost['post_content'];
-			}
+		} else if (wp_text_diff($savedPost['post_content'], $data['post_content'])) {
+
+			// Save the conflicted data in a transient based on the current post ID and current author ID
+			set_transient($transient, stripslashes($data['post_content']), WEEK_IN_SECONDS);
+
+			// Revert to saved version
+			$data['post_content'] = $savedPost['post_content'];
 		}
 
 		return $data;
@@ -99,36 +108,22 @@ class Plugin {
 		}
 	}
 
-	public function alterText($translation, $text) {
-		if ($text == 'Update') {
-			global $post;
-			if (!$post) {
-				return $translation;
-			}
-			$transient = 'conflict_' . $post->ID . '_' . get_current_user_id();
-			if (get_transient($transient)) {
-				return 'Resolve Merge Conflict';
-			}
-		}
-		return $translation;
-	}
-
 	public function renderDiff($left, $right) {
 		$args = array('title' => 'Your edit clashes with a recent edit by another author: please resolve the conflict and re-publish', 'title_left' => 'Currently published version', 'title_right' => 'Your version');
 
 		if (!class_exists( 'WP_Text_Diff_Renderer_Table', false)) {
 			require(ABSPATH . WPINC . '/wp-diff.php');
 		}
-		$left  = normalize_whitespace($left);
+		$left = normalize_whitespace($left);
 		$right = normalize_whitespace($right);
 
-		$left  = explode("\n", $left);
+		$left = explode("\n", $left);
 		$right = explode("\n", $right);
 		$diff = new \Text_Diff($left, $right);
-		$renderer  = new \WP_Text_Diff_Renderer_Table($args);
+		$renderer = new \WP_Text_Diff_Renderer_Table($args);
 		$diff = $renderer->render($diff);
 
-		$r  = "<table class='diff'>\n";
+		$r = "<table class='diff'>\n";
 		$r .= "<col class='content diffsplit left' /><col class='content diffsplit middle' /><col class='content diffsplit right' />";
 		if ($args['title'] || $args['title_left'] || $args['title_right']) {
 			$r .= "<thead>";
@@ -149,6 +144,20 @@ class Plugin {
 		$r .= "</table>";
 		$r .= '<style>table.diff { margin: 2rem 0; background-color: #fff; padding: 1rem 2rem; } table.diff td, table.diff th { font-family: inherit; }</style>';
 		return $r;
+	}
+
+	public function alterText($translation, $text) {
+		if ($text == 'Update') {
+			global $post;
+			if (!$post) {
+				return $translation;
+			}
+			$transient = 'conflict_' . $post->ID . '_' . get_current_user_id();
+			if (get_transient($transient)) {
+				return 'Resolve Merge Conflict';
+			}
+		}
+		return $translation;
 	}
 }
 
