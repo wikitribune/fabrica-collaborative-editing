c2d4c78d6a31174986a2576e28a6ea83908eedd1
Andrew Staffell <andrew@yeswework.com>
2017-10-15 17:49:29 +0200

    fix JS bug

--- js/post.js
+++ js/post.js
@@ -1,63 +1,65 @@
 (function($) {
 	$(function() {
 
-	// Increase heartbeat to 5 seconds
-	wp.heartbeat.interval('fast');
+		// Increase heartbeat to 5 seconds
+		wp.heartbeat.interval('fast');
 
-	// Send post ID with first tick
-	wp.heartbeat.enqueue('fce', { 'post_id' : jQuery('#post_ID').val() }, true);
-
-	// Listen for Heartbeat repsonses
-	$(document).on('heartbeat-tick.fce', function(e, data) {
-
-		// Re-send the post ID with each subsequent tick
+		// Send post ID with first tick
 		wp.heartbeat.enqueue('fce', { 'post_id' : jQuery('#post_ID').val() }, true);
 
-		// [DEBUG] Log response
-		console.log(data);
+		// Listen for Heartbeat repsonses
+		$(document).on('heartbeat-tick.fce', function(e, data) {
 
-		// Check if revision has been update
-		// [TODO] Force conflict resolution immediately
-		/* if (data.fce.last_revision_id != $('#fce_last_revision_id').val()) {
-			console.log('A new revision has been published while you have been editing.');
-		} */
-	});
+			// Re-send the post ID with each subsequent tick
+			wp.heartbeat.enqueue('fce', { 'post_id' : jQuery('#post_ID').val() }, true);
 
-	// Remove old version contents from diff when copying
-	$(document).bind('copy', function(event) {
-		if (!window.getSelection) { return; }
-		var selection = window.getSelection();
-		if (selection.rangeCount <= 0) { return; }
-		event.preventDefault();
+			// [DEBUG] Log response
+			console.log(data);
 
-		// Remove unwanted DOM elements from selection
-		range = selection.getRangeAt(0);
-		var $clonedSelection = $(range.cloneContents());
-		$('.diff-left-side, .diff-divider', $clonedSelection).detach();
-		event.originalEvent.clipboardData.setData('text/plain', $clonedSelection.text());
+			// Check if revision has been update
+			// [TODO] Force conflict resolution immediately
+			/* if (data.fce.last_revision_id != $('#fce_last_revision_id').val()) {
+				console.log('A new revision has been published while you have been editing.');
+			} */
+		});
 
-		// Create element to get selection HTML from
-		var $div = $('<div></div>');
-		$div.append($clonedSelection);
-		var html = $div.html();
-		event.originalEvent.clipboardData.setData('text/html', html);
-	});
+		// Remove old version contents from diff when copying
+		$(document).bind('copy', function(event) {
+			if (!window.getSelection) { return; }
+			var selection = window.getSelection();
+			if (selection.rangeCount <= 0) { return; }
+			event.preventDefault();
 
-	// Don't show a warning when clicking the Resolve button
-	$('#resolve-edit-conflict').click(function() { $(window).off('beforeunload'); });
+			// Remove unwanted DOM elements from selection
+			range = selection.getRangeAt(0);
+			var $clonedSelection = $(range.cloneContents());
+			console.log($clonedSelection);
+			$('.diff-left-side, .diff-divider', $clonedSelection).detach();
+			event.originalEvent.clipboardData.setData('text/plain', $clonedSelection.text());
 
-	// Activate diff tabs
-	$('.fce-diff-tab').click(function(){
-		if ($(this).hasClass('fce-diff-tab-visual')) {
-			$(this).closest('.fce-diff-pair').find('.fce-diff-tab').removeClass('fce-diff-tab--active');
-			$(this).addClass('fce-diff-tab--active');
-			$(this).closest('.fce-diff-pair').find('.fce-diff').removeClass('fce-diff--active');
-			$(this).closest('.fce-diff-pair').find('.fce-diff-visual').addClass('fce-diff--active');
-		} else if ($(this).hasClass('fce-diff-tab-text')) {
-			$(this).closest('.fce-diff-pair').find('.fce-diff-tab').removeClass('fce-diff-tab--active');
-			$(this).addClass('fce-diff-tab--active');
-			$(this).closest('.fce-diff-pair').find('.fce-diff').removeClass('fce-diff--active');
-			$(this).closest('.fce-diff-pair').find('.fce-diff-text').addClass('fce-diff--active');
-		}
+			// Create element to get selection HTML from
+			var $div = $('<div></div>');
+			$div.append($clonedSelection);
+			var html = $div.html();
+			event.originalEvent.clipboardData.setData('text/html', html);
+		});
+
+		// Don't show a warning when clicking the Resolve button
+		$('#resolve-edit-conflict').click(function() { $(window).off('beforeunload'); });
+
+		// Activate diff tabs
+		$('.fce-diff-tab').click(function(){
+			if ($(this).hasClass('fce-diff-tab-visual')) {
+				$(this).closest('.fce-diff-pair').find('.fce-diff-tab').removeClass('fce-diff-tab--active');
+				$(this).addClass('fce-diff-tab--active');
+				$(this).closest('.fce-diff-pair').find('.fce-diff').removeClass('fce-diff--active');
+				$(this).closest('.fce-diff-pair').find('.fce-diff-visual').addClass('fce-diff--active');
+			} else if ($(this).hasClass('fce-diff-tab-text')) {
+				$(this).closest('.fce-diff-pair').find('.fce-diff-tab').removeClass('fce-diff-tab--active');
+				$(this).addClass('fce-diff-tab--active');
+				$(this).closest('.fce-diff-pair').find('.fce-diff').removeClass('fce-diff--active');
+				$(this).closest('.fce-diff-pair').find('.fce-diff-text').addClass('fce-diff--active');
+			}
+		});
 	});
 })(jQuery);
