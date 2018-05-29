16db023b0a5f75ac4d13d66c768199f30227b356
Andrew Staffell <andrew@yeswework.com>
2018-05-29 18:43:48 +0200

    add license, readme and bump version to 0.1.0 ready for open-sourcing

--- fabrica-collaborative-editing.php
+++ fabrica-collaborative-editing.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Fabrica Collaborative Editing
-Plugin URI:
-Description:
-Version: 0.0.10
+Plugin URI: https://github.com/wikitribune/fabrica-collaborative-editing
+Description: Makes WordPress more Wiki-like by allowing more than one person to edit the same Post, Page, or Custom Post Type at the same time. When edits conflict, helps users to view, compare, and merge changes before saving.
+Version: 0.1.0
 Author: Fabrica
 Author URI: https://fabri.ca/
 Text Domain: fabrica-collaborative-editing
-License: GPL-2.0+
-License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+License: MIT
+License URI: https://opensource.org/licenses/MIT
 */
 
 namespace Fabrica\CollaborativeEditing;
